b3720c28ed277eb3aa441496d038cc4763800f8b
djavorszky <daniel.javorszky@liferay.com>
2017-06-13 12:55:02 +0200

    ex1.12: Modify lissajous server to handle GET params

--- ch1/lissajous/main.go
+++ ch1/lissajous/main.go
@@ -13,74 +13,125 @@ import (
 	"image/color"
 	"image/gif"
 	"io"
+	"log"
 	"math"
 	"math/rand"
+	"net/http"
 	"os"
+	"strconv"
+	"time"
 )
 
 //!-main
 // Packages not needed by version in book.
-import (
-	"log"
-	"net/http"
-	"time"
-)
 
 //!+main
 
-var palette = []color.Color{color.Black, color.RGBA{R: 30, G: 197, B: 3, A: 1}, color.RGBA{255, 0, 0, 1}, color.RGBA{0, 255, 0, 1}, color.RGBA{0, 0, 255, 1}}
+var palette = []color.Color{color.Black, color.RGBA{R: 30, G: 197, B: 3, A: 1}}
 
 const (
-	bgIndex = iota
-	fgIndex
-	redIndex
-	greenIndex
-	blueIndex
+	bgIndex = 0
+	fgIndex = 1
+)
+
+var (
+	cycles  float64 // number of complete x oscillator revolutions
+	res     float64 // angular resolution
+	size    float64 // image canvas covers [-size..+size]
+	nframes int     // number of animation frames
+	delay   int     // delay between frames in 10ms units
 )
 
 func main() {
-	//!-main
-	// The sequence of images is deterministic unless we seed
-	// the pseudo-random number generator using the current time.
-	// Thanks to Randall McPherson for pointing out the omission.
 	rand.Seed(time.Now().UTC().UnixNano())
 
 	if len(os.Args) > 1 && os.Args[1] == "web" {
-		//!+http
 		handler := func(w http.ResponseWriter, r *http.Request) {
+			cycles = 5
+			res = 0.001
+			size = 100
+			nframes = 64
+			delay = 8
+
+			if err := r.ParseForm(); err != nil {
+				http.Error(w, "Failed parsing form: "+err.Error(), http.StatusInternalServerError)
+				return
+			}
+
+			fCycles := r.FormValue("cycles")
+			if fCycles != "" {
+				intFCycles, err := strconv.Atoi(fCycles)
+				if err != nil {
+					http.Error(w, "Failed parsing form: "+err.Error(), http.StatusBadRequest)
+				}
+
+				cycles = float64(intFCycles)
+			}
+
+			fRes := r.FormValue("res")
+			if fRes != "" {
+				intFRes, err := strconv.Atoi(fRes)
+				if err != nil {
+					http.Error(w, "Failed parsing form: "+err.Error(), http.StatusBadRequest)
+				}
+
+				res = float64(intFRes)
+			}
+
+			size := r.FormValue("size")
+			if size != "" {
+				intSize, err := strconv.Atoi(size)
+				if err != nil {
+					http.Error(w, "Failed parsing form: "+err.Error(), http.StatusBadRequest)
+				}
+
+				cycles = float64(intSize)
+			}
+
+			fNframes := r.FormValue("nframes")
+			if fNframes != "" {
+				intFNframes, err := strconv.Atoi(fNframes)
+				if err != nil {
+					http.Error(w, "Failed parsing form: "+err.Error(), http.StatusBadRequest)
+				}
+
+				nframes = intFNframes
+			}
+
+			fDelay := r.FormValue("delay")
+			if fDelay != "" {
+				intfDelay, err := strconv.Atoi(fDelay)
+				if err != nil {
+					http.Error(w, "Failed parsing form: "+err.Error(), http.StatusBadRequest)
+				}
+
+				delay = intfDelay
+			}
+
 			lissajous(w)
 		}
 		http.HandleFunc("/", handler)
-		//!-http
+
 		log.Fatal(http.ListenAndServe("localhost:8000", nil))
 		return
 	}
-	//!+main
+
 	lissajous(os.Stdout)
 }
 
 func lissajous(out io.Writer) {
-	const (
-		cycles  = 5     // number of complete x oscillator revolutions
-		res     = 0.001 // angular resolution
-		size    = 100   // image canvas covers [-size..+size]
-		nframes = 64    // number of animation frames
-		delay   = 8     // delay between frames in 10ms units
-	)
 	freq := rand.Float64() * 3.0 // relative frequency of y oscillator
 	anim := gif.GIF{LoopCount: nframes}
 	phase := 0.0 // phase difference
 	for i := 0; i < nframes; i++ {
-		rect := image.Rect(0, 0, 2*size+1, 2*size+1)
+		rect := image.Rect(0, 0, int(2*size+1), int(2*size+1))
 		img := image.NewPaletted(rect, palette)
 		for t := 0.0; t < cycles*2*math.Pi; t += res {
 			x := math.Sin(t)
 			y := math.Sin(t*freq + phase)
 
-			randIndex := uint8(rand.Int()%4 + 1)
-
-			img.SetColorIndex(size+int(x*size+0.5), size+int(y*size+0.5),
-				randIndex)
+			img.SetColorIndex(int(size+x*size+0.5), int(size+y*size+0.5),
+				fgIndex)
 		}
 		phase += 0.1
 		anim.Delay = append(anim.Delay, delay)
